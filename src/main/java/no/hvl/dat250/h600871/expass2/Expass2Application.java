ead4e568827c94323182f5ddabc205bbbe55ca7f
Lars Magnus Nordeide <600871@stud.hvl.no>
2024-09-08 18:10:41 +0200

    Init

---
+++ src/main/java/no/hvl/dat250/h600871/expass2/Expass2Application.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.h600871.expass2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expass2Application.class, args);
+    }
+
+}
---
+++ src/test/java/no/hvl/dat250/h600871/expass2/Expass2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.h600871.expass2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
